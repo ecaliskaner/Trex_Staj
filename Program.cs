c27c52089b04ebd73f3cb502b7a0e9b0184a9df4
Efe Caliskaner <efe.caliskaner@sabanciuniv.edu>
2025-07-01 19:01:25 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Trex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write/Read 
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Alt Satıra İndririr");
+            
+            #endregion
+            #region Variables
+            int plaka = 16; // Değişken tanımlama
+            string sehir = "Bursa"; // String değişken tanımlama
+
+            Console.WriteLine(sehir + " plakası " + plaka); // Değişkeni yazdırma
+            #endregion
+            #region input
+            Console.Write("İsminizi Giriniz: ");
+            string isim = Console.ReadLine(); // Kullanıcıdan veri alma
+            Console.Write("Yaşınızı Giriniz: ");
+            int sayi = int.Parse(Console.ReadLine()); // Kullanıcıdan sayı alma ve string'ten int'e dönüştürme
+            Console.WriteLine(isim + ": " + " yaş: " + sayi); // Kullanıcıdan alınan veriyi yazdırma
+            #endregion
+        }
+    }
+}
