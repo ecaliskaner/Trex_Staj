08e4f60f6753931f6095c8a868617752eefebf6e
Efe Caliskaner <efe.caliskaner@sabanciuniv.edu>
2025-07-01 19:21:38 +0300

    Update Program.cs with foreach example and input sections

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Trex
             #region Write/Read 
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Alt Satıra İndririr");
-            
+
             #endregion
             #region Variables
             int plaka = 16; // Değişken tanımlama
@@ -24,6 +24,13 @@ namespace Trex
             int sayi = int.Parse(Console.ReadLine()); // Kullanıcıdan sayı alma ve string'ten int'e dönüştürme
             Console.WriteLine(isim + ": " + " yaş: " + sayi); // Kullanıcıdan alınan veriyi yazdırma
             #endregion
+            #region Foreach
+            string[] cities = { "Paris", "Londra", "Berlin", "New York" };
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city); // Dizi elemanlarını yazdırma
+            }
+            
         }
     }
 }
